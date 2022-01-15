8a0200d800ae540a7b527c2922fbf4f9eaf275e0
Nikodus <niklas@loidls.com>
2022-01-15 18:47:26 +0100

    Project

--- app/src/main/java/com/example/vic2countrypicker10/MainActivity.java
+++ app/src/main/java/com/example/vic2countrypicker10/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.vic2countrypicker10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView.findViewById(R.id.textview);
+        textView.setText("HELLO from GitHub!");
     }
 }
