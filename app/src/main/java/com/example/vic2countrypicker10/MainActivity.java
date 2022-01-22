8d3730f05b491d42b55adcd81d3ceba7bc430edc
Nikodus <niklas@loidls.com>
2022-01-22 20:13:47 +0100

    Project

--- app/src/main/java/com/example/vic2countrypicker10/MainActivity.java
+++ app/src/main/java/com/example/vic2countrypicker10/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView.findViewById(R.id.textview);
-        textView.setText("I love Yung Spinach Cumshot!");
+        textView.setText("Mattl du bist a Nega!");
     }
 }
